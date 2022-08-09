d70cdd231b3518c40e501931219eb19947098b9c
Michael Braley <michael.parker.braley@gmail.com>
2022-08-09 10:49:27 -0500

    Add variables to script.js
    Added what I believe is all the required const variables for the game. Also variables link to functions which have not been created yet.

---
+++ rock_paper_scissors/scripts/script.js
@@ -0,0 +1,22 @@
+// Variables in this section will be applied to game logic through event listeners contained on ROCK, PAPER, and SCISSORS.
+
+const SELECTROCK = "rock"
+const SELECTPAPER = "paper"
+const SELECTSCISSORS = "scissors"
+
+//Variables in this section will initiate the game and pass on the variables in the previous section.
+
+const ROCK = document.getElementById("rock").addEventListener('click', roundInitiate(ROCK));
+const PAPER = document.getElementById("paper").addEventListener('click', roundInitiate(PAPER));
+const SCISSORS = document.getElementById("scissors").addEventListener('click', roundInitiate(SCISSORS));
+
+//This variable will call a function to reset the entire game.
+
+const RESET = document.getElementById("reset").addEventListener('click', reset());
+
+//Variables in this section will be used to update the game values in the index.html file.
+
+const ROUNDS = getElementById("rounds");
+const WINS = getElementById("wins");
+const LOSSES = getElementById("losses")
+const DRAWS = getElementById("draws");
