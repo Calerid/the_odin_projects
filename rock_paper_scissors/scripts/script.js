46c34d556e791df14b727be2431da20edb73b09a
Michael Braley <michael.parker.braley@gmail.com>
2022-08-09 16:56:12 -0500

    Refactor DOM variables, move event listener
    Refactored the variables so that they now only query the dom for the ids, no longer call for an event listener inside the variable. This was causing a reference error. The event listeners are now seperate and moved about the functions.

--- rock_paper_scissors/scripts/script.js
+++ rock_paper_scissors/scripts/script.js
@@ -4,9 +4,9 @@ const gameChoices = ["rock", "paper", "scissors"];
 
 //Variables in this section will initiate the game and pass on the variables in the previous section.
 
-const ROCK = document.getElementById("rock").addEventListener('click', roundInitiate(gameChoices[0]));
-const PAPER = document.getElementById("paper").addEventListener('click', roundInitiate(gameChoices[1]));
-const SCISSORS = document.getElementById("scissors").addEventListener('click', roundInitiate(gameChoices[2]));
+const ROCK = document.getElementById("rock");
+const PAPER = document.getElementById("paper");
+const SCISSORS = document.getElementById("scissors");
 
 //This variable will call a function to reset the entire game.
 
@@ -25,10 +25,12 @@ let wins = 0;
 let losses = 0;
 let draws = 0;
 
+ROCK.addEventListener('click', roundInitiate(gameChoices[0]))
+PAPER.addEventListener('click', roundInitiate(gameChoices[1]))
+SCISSORS..addEventListener('click', roundInitiate(gameChoices[2]))
 
 function roundInitiate(playerChoice, rounds){
     let computerChoice = gameChoices[Math.floor(Math.random() * 2)];
-    roundCounter(rounds)
     return choiceComparison(playerChoice, computerChoice)
 };
 
