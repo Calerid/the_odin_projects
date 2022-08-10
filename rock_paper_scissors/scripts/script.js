0cbc147e2fed48015a0e9159c2173bc6bfd82c6d
Michael Braley <michael.parker.braley@gmail.com>
2022-08-10 15:29:40 -0500

    Add let variable
    Added the list of variables declared as let under the const variables. These will be number type and update per round win/loss and the applied to html, as well as being used to determine the winner.

--- rock_paper_scissors/scripts/script.js
+++ rock_paper_scissors/scripts/script.js
@@ -2,3 +2,9 @@ const ROUND_H1 = document.getElementById("round");
 const WIN_H2 = document.getElementById("win");
 const LOSS_H2 = document.getElementById("loss");
 const DRAW_H2 = document.getElementById("draw");
+
+
+let round = 0;
+let wins = 0;
+let loss = 0;
+let draws = 0;
