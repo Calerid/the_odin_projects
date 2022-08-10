0793515cdb725de60e227656bebddad54b1e194e
Michael Braley <michael.parker.braley@gmail.com>
2022-08-10 15:25:47 -0500

    Add const variable
    Added several const variables which will be used for updating the html content throughout rounds.

---
+++ rock_paper_scissors/scripts/script.js
@@ -0,0 +1,4 @@
+const ROUND_H1 = document.getElementById("round");
+const WIN_H2 = document.getElementById("win");
+const LOSS_H2 = document.getElementById("loss");
+const DRAW_H2 = document.getElementById("draw");
