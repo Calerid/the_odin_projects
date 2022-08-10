c3a9f46d67a48a121d87948d5658bb0cfc45ea4b
Michael Braley <michael.parker.braley@gmail.com>
2022-08-10 16:23:16 -0500

    Add beginRound() function
    Added a function called begin round which will call roundCounter(round) to update the roundcount. This function will be updated later to call game logic functions

--- rock_paper_scissors/scripts/script.js
+++ rock_paper_scissors/scripts/script.js
@@ -1,12 +1,24 @@
+const ROCK_BUTTON = document.getElementById("rock");
+const PAPER_BUTTON = document.getElementById("paper");
+const SCISSORS_BUTTON = document.getElementById("scissors");
+
 const ROUND_H1 = document.getElementById("round");
 const WIN_H2 = document.getElementById("win");
 const LOSS_H2 = document.getElementById("loss");
 const DRAW_H2 = document.getElementById("draw");
 
-
-let round = 0;
+var round = 0;
 let wins = 0;
 let loss = 0;
 let draws = 0;
 
 const gameChoices = ["rock", "paper", "scissors"];
+
+ROCK_BUTTON.addEventListener("click", beginRound(gameChoices[0]));
+PAPER_BUTTON.addEventListener("click", beginRound(gameChoices[1]));
+SCISSORS_BUTTON.addEventListener("click", beginRound(gameChoices[2]));
+
+function beginRound(gameChoices, round){
+    let computerChoice = Math.floor(Math.random() * 2);
+    roundCounter(round);
+}
