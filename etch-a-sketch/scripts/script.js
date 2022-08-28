3d843aa89bc729c0280a51340f15a452467b8278
Michael Braley <michael.parker.braley@gmail.com>
2022-08-27 21:30:08 -0500

    Add gridCreate()
    Added the function instructions for gridCreate(). This function is called when the event listener fires on a change from gridSelect. The grid creates a new div in a loop and adds it as a child with the class .box in the gridContainer. This is still a WIP with the core function being present.

--- etch-a-sketch/scripts/script.js
+++ etch-a-sketch/scripts/script.js
@@ -1,8 +1,20 @@
 const gridContainer = document.getElementById("grid-container");
 const gridSelect = document.getElementById("grid-value");
 
-gridSelect.addEventListener('select', createGrid(e));
+gridSelect.addEventListener('change', () => createGrid(gridSelect.value));
 
 function createGrid(e){
+    gridErase();
+    console.log(e);
+    for (let i = 0; i < e; i++){
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute('id', `${i}`)
+        newDiv.setAttribute('class', 'box');
+        gridContainer.appendChild(newDiv);
+    }
+    
+}
 
+function gridErase(){
+    
 }
