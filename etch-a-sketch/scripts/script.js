f6087655701f30daca4831c1a16b922812bf5e3d
Michael Braley <michael.parker.braley@gmail.com>
2022-08-27 21:05:24 -0500

    Add event listener to selection
    Added an event listener which will capture the value of the selection menu. This value is then passed to the currently blank function 'createGrid'

--- etch-a-sketch/scripts/script.js
+++ etch-a-sketch/scripts/script.js
@@ -1,1 +1,8 @@
 const gridContainer = document.getElementById("grid-container");
+const gridSelect = document.getElementById("grid-value");
+
+gridSelect.addEventListener('select', createGrid(e));
+
+function createGrid(e){
+
+}
