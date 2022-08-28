a2b276ce0d1a53bd3bbb13d7e74bf555c3ebaa43
Michael Braley <michael.parker.braley@gmail.com>
2022-08-28 12:24:18 -0500

    Add css rules, change html value
    Added new css rules, switched from a flex container to grid. Changed the html values on the dropdown. This is WIP and the next step will be to create a symetrical grid column template.

--- etch-a-sketch/scripts/script.js
+++ etch-a-sketch/scripts/script.js
@@ -4,17 +4,17 @@ const gridSelect = document.getElementById("grid-value");
 gridSelect.addEventListener('change', () => createGrid(gridSelect.value));
 
 function createGrid(e){
-    gridErase();
     console.log(e);
-    for (let i = 0; i < e; i++){
+    for (let i = 0; i <= e; i++){
         let newDiv = document.createElement('div');
         newDiv.setAttribute('id', `${i}`)
         newDiv.setAttribute('class', 'box');
+        newDiv.textContent = `${i}`;
         gridContainer.appendChild(newDiv);
     }
     
 }
 
 function gridErase(){
-    
+    gridContainer.forEach(gridContainer.removeChild())
 }
