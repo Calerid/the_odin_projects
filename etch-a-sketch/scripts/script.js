2d9d8fc20a411129d4387790113e02f3948b3afc
Michael Braley <michael.parker.braley@gmail.com>
2022-08-28 12:45:03 -0500

    Add deployDefaultGrid() function
    Added a function which automatically deploys a default grid of 16 div squares

--- etch-a-sketch/scripts/script.js
+++ etch-a-sketch/scripts/script.js
@@ -3,6 +3,18 @@ const gridSelect = document.getElementById("grid-value");
 
 gridSelect.addEventListener('change', () => createGrid(gridSelect.value));
 
+deployDefaultGrid();
+
+function deployDefaultGrid(){
+    for (i = 1; i <= 16; i++){
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute('id', `${i}`)
+        newDiv.setAttribute('class', 'box');
+        newDiv.textContent = `${i}`;
+        gridContainer.appendChild(newDiv);
+    }
+}
+
 function createGrid(e){
     console.log(e);
     for (let i = 1; i <= e; i++){
