ae3a8d1dd89aa79f64e3f59251743cff64dc674d
Michael Braley <michael.parker.braley@gmail.com>
2022-08-29 09:49:52 -0500

    Add height/width value
    Added several height/width values to the setGridSize() function and to the dropdown menu in the index.html file. Box value now goes up to 1024

--- etch-a-sketch/scripts/script.js
+++ etch-a-sketch/scripts/script.js
@@ -10,7 +10,6 @@ function deployDefaultGrid() {
     let newDiv = document.createElement("div");
     newDiv.setAttribute("id", `${i}`);
     newDiv.setAttribute("class", "box");
-    newDiv.textContent = `${i}`;
     gridContainer.appendChild(newDiv);
     setGridSize()
   }
@@ -23,7 +22,6 @@ function createGrid(e) {
     let newDiv = document.createElement("div");
     newDiv.setAttribute("id", `${i}`);
     newDiv.setAttribute("class", "box");
-    newDiv.textContent = `${i}`;
     gridContainer.appendChild(newDiv);
   }
   setGridSize();
@@ -50,19 +48,24 @@ setInterval(() => {
 
 function setGridSize() {
   let newBox = gridContainer.querySelectorAll(".box");
-  if (newBox.length == 256) {
-    newBox.forEach((box) => box.style.width = '60px')
+  if (newBox.length == 1024) {
+    newBox.forEach((box) => box.style.height = '11.25px')
+  } else if (newBox.length == 512) {
+    newBox.forEach((box) => box.style.height = '22.5px')
+  } else if (newBox.length == 256) {
+    newBox.forEach((box) => box.style.width = '45px')
+    newBox.forEach((box) => box.style.height = '45px')
   } else if (newBox.length == 128) {
     newBox.forEach((box) => box.style.width = '60px')
-    newBox.forEach((box) => box.style.height = '120px')
+    newBox.forEach((box) => box.style.height = '90px')
   } else if (newBox.length == 64) {
     newBox.forEach((box) => box.style.width = '60px')
-    newBox.forEach((box) => box.style.height = '240px')
+    newBox.forEach((box) => box.style.height = '180px')
   } else if (newBox.length == 32) {
     newBox.forEach((box) => box.style.width = '60px')
-    newBox.forEach((box) => box.style.height = '480px')
+    newBox.forEach((box) => box.style.height = '360px')
   } else if (newBox.length == 16) {
     newBox.forEach((box) => box.style.width = '60px')
-    newBox.forEach((box) => box.style.height = '960px')
+    newBox.forEach((box) => box.style.height = '720px')
   }
 }
