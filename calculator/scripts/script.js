f30b5d66cdc52528255398bd98bd8411ee2a27b2
Michael Braley <michael.parker.braley@gmail.com>
2022-09-07 22:47:03 -0500

    Add sorting function & mathematical function
    Added several new functions which will perform mathematical operations when called. These are WIP and are being used to test passing variable data currently.A central sorting function checks if the value prior to last entered in the calculator was an operator - it then calls the appropriate function

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -4,11 +4,53 @@ const BUTTON = document.querySelectorAll(".btn");
 var inputValues = [];
 
 BUTTON.forEach((e) => {
-    e.addEventListener('click', function(){
-        storage(e);
-    });
+  e.addEventListener("click", function () {
+    storage(e);
+    screenUpdate(e);
+  });
 });
 
-function storage(e){
-    inputValues.push(e.value);
+//Think about implementing a queue that pulls the first object
+// that was entered. This will then be evaluated for number vs
+// operator.
+
+function storage(e) {
+  inputValues.push(e.value);
+  if (inputValues.length >= 2){
+  checkValue(inputValues);
+  }
+}
+
+function screenUpdate(e) {
+    let addToScreen = document.createTextNode(e.value);
+    SCREEN.textContent 
+}
+
+function checkValue(inputValues){
+    if (inputValues[inputValues.length-2] === '+'){
+        addition(inputValues)
+    } else if (inputValues[inputValues.length-2] === '-'){
+        subtraction(inputValues)
+    } else if (inputValues[inputValues.length-2] === '/'){
+        division(inputValues)
+    } else if (inputValues[inputValues.length-2] ==='*'){
+        multiplication(inputValues)
+    } else if (inputValues[inputValues.length-2] === "."){
+        decimal(inputValues)
+}
+}
+function addition(inputValues){
+    console.log(inputValues[inputValues.length-2]);
+}
+function subtraction(inputValues){
+    console.log(inputValues[inputValues.length-2]);
+}
+function division(inputValues){
+    console.log(inputValues[inputValues.length-2]);
+}
+function multiplication(inputValues){
+    console.log(inputValues[inputValues.length-2]);
+}
+function decimal(inputValues){
+    console.log(inputValues[inputValues.length-2]);
 }
