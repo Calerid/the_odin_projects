85251ebfea1d568bd575c635a92bd14b047108ef
Michael Braley <michael.parker.braley@gmail.com>
2022-09-09 15:27:41 -0500

    Add cinditional check to  checkValue for '.'
    Added a conditional statement that checks if both the current and last value input are decimal points. If so, it is removed from the array via pop().

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -10,47 +10,44 @@ BUTTON.forEach((e) => {
   });
 });
 
-//Think about implementing a queue that pulls the first object
-// that was entered. This will then be evaluated for number vs
-// operator.
-
 function storage(e) {
   inputValues.push(e.value);
-  if (inputValues.length >= 2){
-  checkValue(inputValues);
+  if (inputValues.length >= 2) {
+    checkValue(inputValues);
   }
 }
 
-function screenUpdate(e) {
-    let addToScreen = document.createTextNode(e.value);
-    SCREEN.textContent 
-}
+function screenUpdate(e) {}
 
-function checkValue(inputValues){
-    if (inputValues[inputValues.length-2] === '+'){
-        addition(inputValues)
-    } else if (inputValues[inputValues.length-2] === '-'){
-        subtraction(inputValues)
-    } else if (inputValues[inputValues.length-2] === '/'){
-        division(inputValues)
-    } else if (inputValues[inputValues.length-2] ==='*'){
-        multiplication(inputValues)
-    } else if (inputValues[inputValues.length-2] === "."){
-        decimal(inputValues)
+function checkValue(inputValues) {
+  if (inputValues[inputValues.length - 2] === ".") {
+    if (inputValues[-1] === ".") {
+      inputValues.pop(".");
+    } else {
+      decimal(inputValues);
+    }
+  } else if (inputValues[inputValues.length - 2] === "+") {
+    addition(inputValues);
+  } else if (inputValues[inputValues.length - 2] === "-") {
+    subtraction(inputValues);
+  } else if (inputValues[inputValues.length - 2] === "/") {
+    division(inputValues);
+  } else if (inputValues[inputValues.length - 2] === "*") {
+    multiplication(inputValues);
+  }
 }
+function addition(inputValues) {
+  console.log(inputValues[inputValues.length - 2]);
 }
-function addition(inputValues){
-    console.log(inputValues[inputValues.length-2]);
+function subtraction(inputValues) {
+  console.log(inputValues[inputValues.length - 2]);
 }
-function subtraction(inputValues){
-    console.log(inputValues[inputValues.length-2]);
+function division(inputValues) {
+  console.log(inputValues[inputValues.length - 2]);
 }
-function division(inputValues){
-    console.log(inputValues[inputValues.length-2]);
+function multiplication(inputValues) {
+  console.log(inputValues[inputValues.length - 2]);
 }
-function multiplication(inputValues){
-    console.log(inputValues[inputValues.length-2]);
-}
-function decimal(inputValues){
-    console.log(inputValues[inputValues.length-2]);
+function decimal(inputValues) {
+  console.log(inputValues[inputValues.length - 2]);
 }
