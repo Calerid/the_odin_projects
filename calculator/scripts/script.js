b05d78140b56b997e2776c79426a4badf0d39c75
Michael Braley <michael.parker.braley@gmail.com>
2022-09-10 14:13:59 -0500

    Remove functions
    Removed functions below screenUpdate(). Trying new approach to the problem and these functions were not appropriate for new method

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -19,35 +19,4 @@ function storage(e) {
 
 function screenUpdate(e) {}
 
-function checkValue(inputValues) {
-  if (inputValues[inputValues.length - 2] === ".") {
-    if (inputValues[-1] === ".") {
-      inputValues.pop(".");
-    } else {
-      decimal(inputValues);
-    }
-  } else if (inputValues[inputValues.length - 2] === "+") {
-    addition(inputValues);
-  } else if (inputValues[inputValues.length - 2] === "-") {
-    subtraction(inputValues);
-  } else if (inputValues[inputValues.length - 2] === "/") {
-    division(inputValues);
-  } else if (inputValues[inputValues.length - 2] === "*") {
-    multiplication(inputValues);
-  }
-}
-function addition(inputValues) {
-  console.log(inputValues[inputValues.length - 2]);
-}
-function subtraction(inputValues) {
-  console.log(inputValues[inputValues.length - 2]);
-}
-function division(inputValues) {
-  console.log(inputValues[inputValues.length - 2]);
-}
-function multiplication(inputValues) {
-  console.log(inputValues[inputValues.length - 2]);
-}
-function decimal(inputValues) {
-  console.log(inputValues[inputValues.length - 2]);
-}
+
